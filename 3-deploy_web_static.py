0426161b2eeeddcf8a0af259864f101eacce44cf
ayadeleke <a.adeleke@alustudent.com>
2023-10-12 18:38:33 -0700

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['54.226.202.30', '3.95.238.193']
 
 
 def do_pack():
